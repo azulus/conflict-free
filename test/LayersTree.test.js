c53e3bcb41952d1464072f808ac7b5c2bd0be5f7
Jeremy Stanley <github@azulus.com>
2016-04-04 01:12:30 -0700

    Started building out LayersTree using compositing of CRDT types

--- test/LayersTree.test.js
+++ test/LayersTree.test.js
@@ -1,22 +1,29 @@
 var assert = require('assert');
 
-var IdGenerator = require('../lib/IdGenerator');
 var LayersTree = require('../lib/LayersTree');
+
+var ID_LENGTH = 4;
 
 describe('LayersTree', function() {
   var tree1, tree2, tree3;
 
-  it("should add and remove", function() {
+  it("should contruct tree", function() {
     tree1 = new LayersTree('a');
 
-    tree1.add('a', null, 5);
-    tree1.add('aa', 'a', 5);
-    tree1.add('aaa', 'aa', 5);
-    tree1.add('ab', 'a', 5);
-    tree1.add('aba', 'ab', 5);
+    tree1.add('a', null, {name: 'Root Layer'});
 
-    // console.log(tree1.getState());
+    tree1.add('aa', 'a', {name: 'Backgrounds'});
+    tree1.add('aac', 'aa', {name: 'Text'});
+    tree1.add('aab', 'aa', {name: 'Circles'}, null, 'aac');
+    tree1.add('aaa', 'aa', {name: 'Logo'}, 'aab', 'aac');
 
-    tree1.remove('aa');
+    tree1.add('ac', 'a', {name: 'Details'}, 'aa', null);
+    tree1.add('aca', 'ac', {name: 'Items'});
+
+    tree1.add('ab', 'a', {name: 'Mapping'}, 'aa', 'ac');
+    tree1.add('aba', 'ab', {name: 'Building'});
+    tree1.add('abac', 'aba', {name: 'Sitting Room'});
+    tree1.add('abab', 'aba', {name: 'Kitchen'}, null, 'abac');
+    tree1.add('abaa', 'aba', {name: 'Entryway'}, null, 'abab');
   });
 });
