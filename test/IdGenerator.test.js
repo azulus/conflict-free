2fb6b5a4cac8e1c4ea6d9a2e8e499e8a84a1567a
Jeremy Stanley <github@azulus.com>
2016-04-10 14:56:34 -0700

    Fixed IdGenerator infinite nesting issue

--- test/IdGenerator.test.js
+++ test/IdGenerator.test.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 
 var IdGenerator = require('../lib/IdGenerator');
 
+var EXPECTED_ID_PATTERN = /^(?:[a-zA-Z0-9\:\;]+(?:\.[a-z0-9\#]*)\.*?)(?:\/[a-zA-Z0-9\:\;]+(?:\.[a-z0-9\#]*)?\.*)*\/$/;
 var TEST_ITERATIONS = 100;
 var ID_LENGTH = 4;
 
@@ -10,8 +11,8 @@ describe('IdGenerator', function() {
   var max = IdGenerator.getMax(ID_LENGTH);
 
   it("should return valid min and max", function() {
-    assert.equal(IdGenerator.stringify(min), '++++');
-    assert.equal(IdGenerator.stringify(max), 'zzzz');
+    assert.equal(IdGenerator.stringify(min), '0000/');
+    assert.equal(IdGenerator.stringify(max), 'zzzz/');
   });
 
   it("should convert between number and string", function() {
@@ -23,8 +24,13 @@ describe('IdGenerator', function() {
   });
 
   it("should generate id of length", function() {
-    var id = IdGenerator.generate('siteA', ID_LENGTH);
-    assert.equal(IdGenerator.stringify(id).length, ID_LENGTH + 6);
+    var siteDisambiguator = 'siteA';
+    var id = IdGenerator.generate(siteDisambiguator, ID_LENGTH);
+    var expectedLength = siteDisambiguator.length +
+      1 + /* Disambiguator delimiter */
+      1 + /* Trailing slash */
+      ID_LENGTH;
+    assert.equal(IdGenerator.stringify(id).length, expectedLength, IdGenerator.stringify(id) + " should have length of " + expectedLength);
   });
 
   it("should generate ids between undefined and undefined", function() {
@@ -68,30 +74,30 @@ describe('IdGenerator', function() {
     var maxId = [minId[0], 'siteB'];
     for (var i = 0; i < TEST_ITERATIONS; i++) {
       var id = IdGenerator.generate('siteA', ID_LENGTH, minId, maxId);
-      assert.equal(IdGenerator.compare(minId, id), -1);
-      assert.equal(IdGenerator.compare(maxId, id), 1);
+      assert.equal(IdGenerator.compare(minId, id), -1, IdGenerator.stringify(minId) + ' < ' + IdGenerator.stringify(id));
+      assert.equal(IdGenerator.compare(maxId, id), 1, IdGenerator.stringify(id) + ' < ' + IdGenerator.stringify(maxId));
     }
   });
 
   it("should generate ids between id-siteA/aaaa and id-siteB/aaaa", function() {
     var minId = IdGenerator.generate('siteA', ID_LENGTH);
     var maxId = [].concat(minId);
-    minId[0] += ':siteA/aaaa';
-    maxId[0] += ':siteB/aaaa';
+    minId[0] += '.siteA/aaaa';
+    maxId[0] += '.siteB/aaaa';
     for (var i = 0; i < TEST_ITERATIONS; i++) {
       var id = IdGenerator.generate('siteA', ID_LENGTH, minId, maxId);
-      assert.equal(IdGenerator.compare(minId, id), -1);
-      assert.equal(IdGenerator.compare(maxId, id), 1);
+      assert.equal(IdGenerator.compare(minId, id), -1, IdGenerator.stringify(minId) + ' < ' + IdGenerator.stringify(id));
+      assert.equal(IdGenerator.compare(maxId, id), 1), IdGenerator.stringify(id) + ' < ' + IdGenerator.stringify(maxId);
     }
   });
 
-  it("should generate ids between id and id:siteB/aaaa", function() {
+  it("should generate ids between id and id!siteB/aaaa", function() {
     var minId = IdGenerator.generate('siteA', ID_LENGTH);
-    var maxId = [minId[0] + ':siteB/aaaa', 'siteB'];
+    var maxId = [minId[0] + '.siteB/aaaa', 'siteB'];
     for (var i = 0; i < TEST_ITERATIONS; i++) {
       var id = IdGenerator.generate('siteA', ID_LENGTH, minId, maxId);
-      assert.equal(IdGenerator.compare(minId, id), -1);
-      assert.equal(IdGenerator.compare(maxId, id), 1);
+      assert.equal(IdGenerator.compare(minId, id), -1, IdGenerator.stringify(minId) + ' < ' + IdGenerator.stringify(id));
+      assert.equal(IdGenerator.compare(maxId, id), 1, IdGenerator.stringify(id) + ' < ' + IdGenerator.stringify(maxId));
     }
   });
 
@@ -101,52 +107,53 @@ describe('IdGenerator', function() {
     var maxId = IdGenerator.generate('siteA', ID_LENGTH, minId, null);
     for (var i = 0; i < TEST_ITERATIONS; i++) {
       var id = IdGenerator.generate('siteA', ID_LENGTH, minId, maxId);
-      assert.equal(IdGenerator.compare(minId, id), -1);
-      assert.equal(IdGenerator.compare(maxId, id), 1);
+      assert.equal(IdGenerator.compare(minId, id), -1, IdGenerator.stringify(minId) + ' < ' + IdGenerator.stringify(id));
+      assert.equal(IdGenerator.compare(maxId, id), 1, IdGenerator.stringify(id) + ' < ' + IdGenerator.stringify(maxId));
     }
   });
 
-  it("should generate ids between id and id:siteA/bbbb", function() {
+  it("should generate ids between id and id!siteA/bbbb", function() {
     var minId = IdGenerator.generate('siteA', ID_LENGTH);
     var maxId = IdGenerator.generate('siteA', ID_LENGTH, minId, null);
-    maxId[0] += ':siteA/bbbb';
+    maxId[0] += '.siteA/bbbb';
     for (var i = 0; i < TEST_ITERATIONS; i++) {
       var id = IdGenerator.generate('siteA', ID_LENGTH, minId, maxId);
-      assert.equal(IdGenerator.compare(minId, id), -1);
-      assert.equal(IdGenerator.compare(maxId, id), 1);
+      assert.equal(IdGenerator.compare(minId, id), -1, IdGenerator.stringify(minId) + ' < ' + IdGenerator.stringify(id));
+      assert.equal(IdGenerator.compare(maxId, id), 1, IdGenerator.stringify(id) + ' < ' + IdGenerator.stringify(maxId));
     }
   });
 
-  it("should generate ids between id:siteA/aaaa and id:siteB/ffff", function() {
+  it("should generate ids between id!siteA/aaaa and id!siteB/ffff", function() {
     var minId = IdGenerator.generate('siteA', ID_LENGTH);
     var maxId = [].concat(minId);
-    minId[0] += ':siteA/aaaa';
-    maxId[0] += ':siteB/ffff';
+    minId[0] += '.siteA/aaaa';
+    maxId[0] += '.siteB/ffff';
     for (var i = 0; i < TEST_ITERATIONS; i++) {
       var id = IdGenerator.generate('siteA', ID_LENGTH, minId, maxId);
-      assert.equal(IdGenerator.compare(minId, id), -1);
-      assert.equal(IdGenerator.compare(maxId, id), 1);
+      assert.equal(IdGenerator.compare(minId, id), -1, IdGenerator.stringify(minId) + ' < ' + IdGenerator.stringify(id));
+      assert.equal(IdGenerator.compare(maxId, id), 1, IdGenerator.stringify(id) + ' < ' + IdGenerator.stringify(maxId));
     }
   });
 
   it("should generate ids between id/subId and id/subId", function() {
     var minId = IdGenerator.generate('siteA', ID_LENGTH);
     var maxId = [].concat(minId);
-    minId[0] += '/aaaa';
-    maxId[0] += '/ffff';
+    minId[0] += '.siteA/aaaa';
+    maxId[0] += '.siteA/ffff';
     for (var i = 0; i < TEST_ITERATIONS; i++) {
       var id = IdGenerator.generate('siteA', ID_LENGTH, minId, maxId);
-      assert.equal(IdGenerator.compare(minId, id), -1);
-      assert.equal(IdGenerator.compare(maxId, id), 1);
+      assert.equal(IdGenerator.compare(minId, id), -1, IdGenerator.stringify(minId) + ' < ' + IdGenerator.stringify(id));
+      assert.equal(IdGenerator.compare(maxId, id), 1, IdGenerator.stringify(id) + ' < ' + IdGenerator.stringify(maxId));
     }
   });
 
   it("should insert a bunch of ids without a problem", function() {
     var sites = ['s0', 's1', 's2', 's3'];
     var ids = [
-      IdGenerator.generate(sites[Math.floor(Math.random() * sites.length)], 2),
-      IdGenerator.generate(sites[Math.floor(Math.random() * sites.length)], 2)
+      IdGenerator.generate(sites[Math.floor(Math.random() * sites.length)], 1),
+      IdGenerator.generate(sites[Math.floor(Math.random() * sites.length)], 1)
     ];
+    var comparator = IdGenerator.compare.bind(IdGenerator);
 
     for (var i = 0; i < 20000; i++) {
       var id1 = ids[Math.floor(Math.random() * ids.length)];
@@ -157,9 +164,14 @@ describe('IdGenerator', function() {
       if (id1String === id2String) {
         i--;
       } else {
-        var newId = IdGenerator.generate(sites[Math.floor(Math.random() * sites.length)], 2, compareVal === -1 ? id1 : id2, compareVal === 1 ? id1 : id2);
-        assert.equal(IdGenerator.compare(id1, newId), compareVal);
-        assert.equal(IdGenerator.compare(newId, id2), compareVal);
+        var randomSite = sites[Math.floor(Math.random() * sites.length)];
+        var minId = compareVal === -1 ? id1 : id2;
+        var maxId = compareVal === -1 ? id2 : id1;
+        var newId = IdGenerator.generate(randomSite, 1, minId, maxId);
+        var newIdString = IdGenerator.stringify(newId);
+        assert.equal(Array.isArray(newIdString.match(EXPECTED_ID_PATTERN)), true, newIdString + "should match the id pattern");
+        assert.equal(IdGenerator.compare(id1, newId), compareVal, newIdString + " should be between " + IdGenerator.stringify(minId) + " and " + IdGenerator.stringify(maxId));
+        assert.equal(IdGenerator.compare(newId, id2), compareVal, newIdString + " should be between " + IdGenerator.stringify(minId) + " and " + IdGenerator.stringify(maxId));
         ids.push(newId);
       }
     }
