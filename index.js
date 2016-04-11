600b8f7b40a133d57e6bee3378cd3a9181603aa4
Jeremy Stanley <github@azulus.com>
2016-04-10 17:20:45 -0700

    Adding LayersTree to exports

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
   IdGenerator: require('./lib/IdGenerator'),
   LastWriterWinsMap: require('./lib/LastWriterWinsMap'),
   LastWriterWinsSet: require('./lib/LastWriterWinsMap'),
+  LayersTree: require('./lib/LayersTree'),
   ObservedRemoveSet: require('./lib/ObservedRemoveSet'),
   PositiveNegativeCounter: require('./lib/PositiveNegativeCounter'),
   TwoPhasesSet: require('./lib/TwoPhasesSet')
